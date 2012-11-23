6b8187375718e50ea94244c764a5f7da1ef585e8
Jason Smale <jsmale@zendesk.com>
2012-11-23 11:42:16 +1100

    Change license to apache 2

--- src/app/code/community/Zendesk/Zendesk/controllers/Adminhtml/ZendeskController.php
+++ src/app/code/community/Zendesk/Zendesk/controllers/Adminhtml/ZendeskController.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Zendesk Magento integration
+ * Copyright 2012 Zendesk.
  *
- * NOTICE OF LICENSE
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This source file is subject to The MIT License (MIT) that is bundled with
- * this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/mit-license.php
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * @copyright Copyright (c) 2012 Zendesk (www.zendesk.com)
- * @license http://opensource.org/licenses/mit-license.php The MIT License (MIT)
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 class Zendesk_Zendesk_Adminhtml_ZendeskController extends Mage_Adminhtml_Controller_Action
