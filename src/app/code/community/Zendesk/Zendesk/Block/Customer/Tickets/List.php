c3ba0b192f44d2c42d32dbf32a352980b71261e2
Martin Hansen <martin.hansen@gmail.com>
2014-04-11 19:36:33 -0300

    Added Tickets and Tickets_List blocks for MyAccount

---
+++ src/app/code/community/Zendesk/Zendesk/Block/Customer/Tickets/List.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Copyright 2012 Zendesk.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class Zendesk_Zendesk_Block_Customer_Tickets_List extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplate('zendesk/customer/tickets/list.phtml');
+    }
+
+
+    protected function _getCustomerSession()
+    {
+        return Mage::getSingleton('customer/session');
+    }
+
+
+    public function getCustomer()
+    {
+        $session = $this->_getCustomerSession();
+        $customer = false;
+
+        if($session) {
+            $customer = $session->getCustomer();
+        }
+
+        return $customer;
+    }
+
+
+    public function getList()
+    {
+        $customer = $this->getCustomer();
+        $tickets = null;
+
+        if($customer) {
+            $tickets = Mage::getModel('zendesk/api_tickets')->forRequester($customer->getEmail());
+        }
+
+        return $tickets;
+    }
+}
