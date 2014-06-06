c82dee39046ad1ad01412bd008544ed4dcff06d4
Martin Hansen <martin.hansen@gmail.com>
2014-06-06 10:16:30 -0300

    replace tabs with spaces

--- src/app/code/community/Zendesk/Zendesk/controllers/ApiController.php
+++ src/app/code/community/Zendesk/Zendesk/controllers/ApiController.php
@@ -27,7 +27,7 @@ class Zendesk_Zendesk_ApiController extends Mage_Core_Controller_Front_Action
 
         if(!$tokenString && isset($_SERVER['Authorization'])) {
             $tokenString = $_SERVER['Authorization'];
-		}
+        }
 
         if(!$tokenString && isset($_SERVER['HTTP_AUTHORIZATION'])) {
             $tokenString = $_SERVER['HTTP_AUTHORIZATION'];
