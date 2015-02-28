512c5de42b0ef408d7f0cff5c99bcca563ed93bd
Miguel Molina <mmolina@gmail.com>
2015-02-27 16:25:52 -0800

    Remove duplicated "zendesk" string on JS snippet

--- src/app/code/community/Zendesk/Zendesk/data/zendesk_setup/data-upgrade-1.4.0-2.0.0.php
+++ src/app/code/community/Zendesk/Zendesk/data/zendesk_setup/data-upgrade-1.4.0-2.0.0.php
@@ -41,9 +41,9 @@ if($domain) {
 
     // The Web Widget code snippet, using the account zendesk domain from settings
     $webWidgetSnippet=<<<EOJS
-<!-- Start of zendesk Zendesk Widget script -->
+<!-- Start of Zendesk Widget script -->
 <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("//assets.zendesk.com/embeddable_framework/main.js","{$domain}");/*]]>*/</script>
-<!-- End of zendesk Zendesk Widget script -->
+<!-- End of Zendesk Widget script -->
 EOJS;
 
     $config->saveConfig('zendesk/frontend_features/web_widget_code_snippet', $webWidgetSnippet);
