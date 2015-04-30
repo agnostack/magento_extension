eb351bd52713fb8b6006726daa184f2605d2b5fd
maxmi <maxmi@maxmi-MS-7817>
2015-04-30 12:19:10 +0200

    Fix PHP Warnings/Notices caused by incorrect types and method overriding signatures

--- src/app/code/community/Zendesk/Zendesk/Block/Adminhtml/Create/Order.php
+++ src/app/code/community/Zendesk/Zendesk/Block/Adminhtml/Create/Order.php
@@ -29,7 +29,7 @@ class Zendesk_Zendesk_Block_Adminhtml_Create_Order extends Mage_Adminhtml_Block_
         return Mage::helper('zendesk')->__('Please Select Order to Add');
     }
     
-    public function getButtonsHtml()
+    public function getButtonsHtml($area = null)
     {
         $addButtonData = array(
             'label' => Mage::helper('zendesk')->__('Select Order'),
--- src/app/code/community/Zendesk/Zendesk/Model/Api/Requesters.php
+++ src/app/code/community/Zendesk/Zendesk/Model/Api/Requesters.php
@@ -17,7 +17,7 @@
 
 class Zendesk_Zendesk_Model_Api_Requesters extends Zendesk_Zendesk_Model_Api_Users
 {
-    public function create($email, $name)
+    public function create($email, $name = null)
     {
         if(!Zend_Validate::is($email, 'EmailAddress')) {
             throw new InvalidArgumentException('Invalid email address provided');
